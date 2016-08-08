c1c4fd7aac3a416fe4263566d64e09a7468f9772
deweitie <dewei.tie@my.jcu.edu.au>
2016-08-08 13:25:07 +1000

    Add files via upload

--- Blogsand book.php
+++ Blogsand book.php
@@ -1,6 +1,4 @@
 
-
-
 <html>
 <head>
   <meta charset="gb231fd">
@@ -26,11 +24,15 @@
   <article>
     <div class="banner">
       <ul class="texts">
+
       </ul>
     </div>
     <div class="bloglist">
       <h2>
-        <p> <?php 
+        <p>Bookname</p>
+      </h2>
+      <div class="blogs">
+     <?php 
        error_reporting(0);
         include('conn.php');
         $result = mysql_query("SELECT * FROM booklist where ID='1'");
@@ -38,58 +40,19 @@
 
         while ($row = mysql_fetch_array($result))
           {
-            echo $row['Bookname'];
+            echo $row['content'];
             echo "<br />";
           }
-          ?></p>
-      </h2>
-      <div class="blogs">
-   <h1>chapter</h1>
-<?php 
-
-include('conn.php');
-$result = mysql_query("SELECT * FROM booklist where Bookname='qwe'");
-//$row = mysql_fetch_array($result);
-//  echo $row['Bookname'];
-//  echo "<br />";
-
-while ($row = mysql_fetch_array($result))
-{
-  echo "<h2>" . "<a href=\"index.php#" . $row['Chapter'] . "\">" . $row['Chapter'] . "</a></h2><br />";
-}
-
-// <a href="#Introduction"> .. </a>
-
- ?>
-
-<a name="#introduction"></a> <?php
- include('conn.php');
-$result = mysql_query("SELECT * FROM booklist where Title='introduction'");
-	while ($row = mysql_fetch_array($result))
-          {
-            echo $row['content'];
-            echo "<br />";
-          } 
- 
- ?>
-<br>
-
-<a name="#123123">
-<?php
- include('conn.php');
-$result = mysql_query("SELECT * FROM booklist where Chapter='123123'");
-  while ($row = mysql_fetch_array($result))
-          {
-            echo $row['content'];
-            echo "<br />";
-          } 
- 
- ?>
-
-      </div>
+          ?>      </div>
     </div>
   </article>
-
+  <aside>
+    <div class="sgin"><a href="file:///C|/Users/junpeng/Desktop/Blog&Book/signin.html"><span>Sign out</span></a></div>
+    <div class="tj_news">
+      <h2>
+        <p class="tj_t1">Book</p>
+      </h2>
+    </div>
 	
 
     <div class="topspaceinfo">
@@ -97,15 +60,13 @@ $result = mysql_query("SELECT * FROM booklist where Chapter='123123'");
       <p></p>
     </div>
     <div class="about_c">
-     
-
-
+  
     </div>
 
   </aside>
   <script src="file:///C|/Users/junpeng/Desktop/Blog&Book/js/silder.js"></script>
   <div class="clear"></div>
-  <!-- Ã‡Ã¥Â³Ã½Â¸Â¡Â¶Â¯ -->
+  <!-- Çå³ý¸¡¶¯ -->
 </div>
 </body>
 </html>
--- chapter.php
+++ chapter.php
@@ -1,6 +1,4 @@
 
-
-
 <html>
 <head>
   <meta charset="gb231fd">
@@ -26,11 +24,12 @@
   <article>
     <div class="banner">
       <ul class="texts">
+
       </ul>
     </div>
     <div class="bloglist">
       <h2>
-        <p> <?php 
+        <p>Bookname <?php 
        error_reporting(0);
         include('conn.php');
         $result = mysql_query("SELECT * FROM booklist where ID='1'");
@@ -44,48 +43,18 @@
           ?></p>
       </h2>
       <div class="blogs">
-   <h1>chapter</h1>
 <?php 
+       error_reporting(0);
+        include('conn.php');
+        $result = mysql_query("SELECT * FROM booklist where ID='1'");
 
-include('conn.php');
-$result = mysql_query("SELECT * FROM booklist where Bookname='qwe'");
-//$row = mysql_fetch_array($result);
-//  echo $row['Bookname'];
-//  echo "<br />";
 
-while ($row = mysql_fetch_array($result))
-{
-  echo "<h2>" . "<a href=\"index.php#" . $row['Chapter'] . "\">" . $row['Chapter'] . "</a></h2><br />";
-}
-
-// <a href="#Introduction"> .. </a>
-
- ?>
-
-<a name="#introduction"></a> <?php
- include('conn.php');
-$result = mysql_query("SELECT * FROM booklist where Title='introduction'");
-	while ($row = mysql_fetch_array($result))
+        while ($row = mysql_fetch_array($result))
           {
             echo $row['content'];
             echo "<br />";
-          } 
- 
- ?>
-<br>
-
-<a name="#123123">
-<?php
- include('conn.php');
-$result = mysql_query("SELECT * FROM booklist where Chapter='123123'");
-  while ($row = mysql_fetch_array($result))
-          {
-            echo $row['content'];
-            echo "<br />";
-          } 
- 
- ?>
-
+          }
+          ?>
       </div>
     </div>
   </article>
@@ -97,15 +66,13 @@ $result = mysql_query("SELECT * FROM booklist where Chapter='123123'");
       <p></p>
     </div>
     <div class="about_c">
-     
-
-
+       
     </div>
 
   </aside>
   <script src="file:///C|/Users/junpeng/Desktop/Blog&Book/js/silder.js"></script>
   <div class="clear"></div>
-  <!-- Ã‡Ã¥Â³Ã½Â¸Â¡Â¶Â¯ -->
+  <!-- Çå³ý¸¡¶¯ -->
 </div>
 </body>
 </html>
